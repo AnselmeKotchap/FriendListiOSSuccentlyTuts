591403129d0f5f76b0797275846215ca6667d70c
Anselme <nkanselme@gmail.com>
2015-07-22 12:19:54 +0200

    Initial Commit

---
+++ FriendListiOSSuccentlyTuts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FriendListiOSSuccentlyTuts
+//
+//  Created by Anselme Kotchap on 2015/07/22.
+//  Copyright (c) 2015 MIND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FriendListiOSSuccentlyTuts/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  FriendListiOSSuccentlyTuts
+//
+//  Created by Anselme Kotchap on 2015/07/22.
+//  Copyright (c) 2015 MIND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
